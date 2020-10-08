2072b356873918328b71ffc2599c16676852af91
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-08 23:04:20 +0200

    finished todo list axios jsonplaceholder router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld :msg="msg" />
-	</div>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
-
+import Header from "./components/layout/Header";
 export default {
-	name: "App",
-	components: {
-		HelloWorld,
-	},
-	data() {
-		return {
-			msg: "Welcome to Your Vue.js App",
-		}
-	},
-}
+  name: "App",
+  components: {
+    Header,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo-item">
+    <div class="check" :class="{ 'is-complete': todo.completed }">
+      <input
+        type="checkbox"
+        style="margin-right : 8px"
+        @change="markComplete"
+        :checked="todo.completed"
+      />
+      <p>{{ todo.title }}</p>
+    </div>
+    <button class="del" @click="$emit('del-todo', todo.id)">
+      X
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+  display: flex;
+  justify-content: space-between;
+}
+.check {
+  display: flex;
+  align-items: center;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
